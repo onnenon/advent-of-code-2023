f469b52d607d20838bca76df324409422d706ea7
Stephen Onnen <stephen.onnen@gmail.com>
2023-12-03 14:48:59 -0600

    formatting

--- lib/AdventOfCode.hs
+++ lib/AdventOfCode.hs
@@ -1,22 +1,96 @@
 module AdventOfCode where
 
-import Data.Char ( isDigit )
-import Data.List ( find )
-import Text.Read ( readMaybe )
-
+import Data.Char (isDigit)
+import Data.List (isInfixOf)
+import Data.Maybe
+import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
     calibrationDoc <- readFile "input.txt"
-    let calibrationLines = lines calibrationDoc 
-    putStrLn $ show $ sum $ map (\x -> case getNumberFromLine x of
-        Nothing -> 0
-        Just value -> value) calibrationLines
-
+    let calibrationLines = lines calibrationDoc
+    print $
+        sum $
+            map
+                ( Data.Maybe.fromMaybe 0 . getNumberFromLine
+                )
+                calibrationLines
 
 -- | Given a string, return the first and last digit as an Int
 getNumberFromLine :: String -> Maybe Int
-getNumberFromLine line = do
-    firstDigit <- find isDigit line
-    lastDigit <- find isDigit $ reverse line
+getNumberFromLine (x : xs) = do
+    firstDigit <- getFirstNumOrWord [x] xs
+    lastDigit <- getLastNumOrWord " " reversed
     readMaybe [firstDigit, lastDigit]
+  where
+    reversed = reverse (x : xs)
+getNumberFromLine [] = Nothing
+
+getFirstNumOrWord :: String -> String -> Maybe Char
+getFirstNumOrWord part []
+    | isDigit lastPart = Just lastPart
+    | "one" `isInfixOf` part = Just '1'
+    | "two" `isInfixOf` part = Just '2'
+    | "three" `isInfixOf` part = Just '3'
+    | "four" `isInfixOf` part = Just '4'
+    | "five" `isInfixOf` part = Just '5'
+    | "six" `isInfixOf` part = Just '6'
+    | "seven" `isInfixOf` part = Just '7'
+    | "eight" `isInfixOf` part = Just '8'
+    | "nine" `isInfixOf` part = Just '9'
+    | otherwise = Nothing
+  where
+    lastPart = last part
+getFirstNumOrWord part (x : xs)
+    | isDigit lastPart = Just lastPart
+    | "one" `isInfixOf` part = Just '1'
+    | "two" `isInfixOf` part = Just '2'
+    | "three" `isInfixOf` part = Just '3'
+    | "four" `isInfixOf` part = Just '4'
+    | "five" `isInfixOf` part = Just '5'
+    | "six" `isInfixOf` part = Just '6'
+    | "seven" `isInfixOf` part = Just '7'
+    | "eight" `isInfixOf` part = Just '8'
+    | "nine" `isInfixOf` part = Just '9'
+    | otherwise = getFirstNumOrWord (part ++ [x]) $ xs
+  where
+    lastPart = last part
+
+getLastNumOrWord :: String -> String -> Maybe Char
+getLastNumOrWord part []
+    | isDigit lastPart = Just lastPart
+    | "one" `isInfixOf` revPart = Just '1'
+    | "two" `isInfixOf` revPart = Just '2'
+    | "three" `isInfixOf` revPart = Just '3'
+    | "four" `isInfixOf` revPart = Just '4'
+    | "five" `isInfixOf` revPart = Just '5'
+    | "six" `isInfixOf` revPart = Just '6'
+    | "seven" `isInfixOf` revPart = Just '7'
+    | "eight" `isInfixOf` revPart = Just '8'
+    | "nine" `isInfixOf` revPart = Just '9'
+    | otherwise = Nothing
+  where
+    lastPart = last part
+    revPart = reverse part
+getLastNumOrWord part (x : xs)
+    | isDigit lastPart = Just lastPart
+    | "one" `isInfixOf` revPart = Just '1'
+    | "two" `isInfixOf` revPart = Just '2'
+    | "three" `isInfixOf` revPart = Just '3'
+    | "four" `isInfixOf` revPart = Just '4'
+    | "five" `isInfixOf` revPart = Just '5'
+    | "six" `isInfixOf` revPart = Just '6'
+    | "seven" `isInfixOf` revPart = Just '7'
+    | "eight" `isInfixOf` revPart = Just '8'
+    | "nine" `isInfixOf` revPart = Just '9'
+    | otherwise = getFirstNumOrWord (part ++ [x]) $ xs
+  where
+    lastPart = last part
+    revPart = reverse part
+
+-- [1, n, i, n, e]
+-- [e]
+-- [e, n]
+-- [e, n, i]
+-- [e, n, i, n]
+-- [e, n, i, n, 1]
--- test/Test.hs
+++ test/Test.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE RecordWildCards #-}
 
-import Data.Foldable     (for_)
-import Test.Hspec        (Spec, describe, it, shouldBe)
+import Data.Foldable (for_)
+import Test.Hspec (Spec, describe, it, shouldBe)
 import Test.Hspec.Runner (defaultConfig, hspecWith)
 
 import AdventOfCode (getNumberFromLine)
@@ -10,26 +10,29 @@ main :: IO ()
 main = hspecWith defaultConfig specs
 
 specs :: Spec
-specs = describe "getNumberFromLIne" $ for_ cases test
+specs = describe "getNumberFromLine" $ for_ cases test
   where
-
     test Case{..} = it explanation assertion
       where
         explanation = unwords [show input, "-", description]
-        assertion   = getNumberFromLine input `shouldBe` expected
+        assertion = getNumberFromLine input `shouldBe` expected
 
-data Case = Case { description :: String
-                 , input       :: String
-                 , expected    :: Maybe Int
-                 }
+data Case = Case
+    { description :: String
+    , input :: String
+    , expected :: Maybe Int
+    }
 
 cases :: [Case]
-cases = [ Case { description = "Correctly parses a line with two digits"
-               , input       = "19qdlpmdrxone7sevennine"
-               , expected    = Just 19
-               },
-          Case { description = "Correctly parses a line with two digits"
-               , input       = "ninefivefive2nine5ntvscdfdsmvqgcbxxxt"
-               , expected    = Just 95
-               }
-        ]
+cases =
+    [ Case
+        { description = "Correctly parses a line with two digits"
+        , input = "19qdlpmdrxone7sevennine"
+        , expected = Just 19
+        }
+    , Case
+        { description = "Correctly parses a line with two digits"
+        , input = "ninefivefive2nine5ntvscdfdsmvqgcbxxxt"
+        , expected = Just 95
+        }
+    ]
