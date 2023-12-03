1b1ad2c4834f8fb6c17587d62bd991385a2f47aa
Stephen Onnen <stephen.onnen@gmail.com>
2023-12-03 12:09:08 -0600

    tests

--- test/Tests.hs
+++
@@ -1,7 +0,0 @@
-import Test.HUnit
-
-runTests :: IO Counts
-runTests = runTestTT $ TestList [testGetNumberFromLine]
-
-testGetNumberFromLine :: Test
-testGetNumberFromLine = TestCase $ assertEqual "for (getNumberFromLine \"123abc456\")," (Just 14) (getNumberFromLine "123abc456")
